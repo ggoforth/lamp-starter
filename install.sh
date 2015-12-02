dfdf1796aba69043cf2756d8aec731d74e2c62c2
Greg Goforth <greg.goforth@gmail.com>
2015-12-02 08:20:20 -0800

    adds gitignore and install.sh

---
+++ install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# simple vagrant provisioning script
+# installs : apache2, php 5.5 and mysql-server and client, node js, and gulp
+
+# some coloring in outputs.
+COLOR="\033[;35m"
+COLOR_RST="\033[0m"
+
+echo -e "${COLOR}---updating system---${COLOR_RST}"
+apt-get update
+
+echo -e "${COLOR}---installing some tools: zip,unzip,curl, python-software-properties---${COLOR_RST}"
+
+apt-get install -y software-properties-common
+apt-get install -y python-software-properties
+apt-get install -y zip unzip
+apt-get install -y curl
+apt-get install -y build-essential
+apt-get install -y vim
+apt-get install -y git
+
+add-apt-repository -y ppa:pi-rho/dev
+
+apt-get update
+apt-get install -y tmux
+apt-get install -y python g++ make
+
+# node js
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+echo "5.0.0" >> ~/.nvmrc
+nvm install
+npm install -g gulp
+
+# installing mysql
+# pre-loading a default password --> yourpassword
+echo -e "${COLOR}---installing MySql---${COLOR_RST}"
+apt-get install -y mysql-server mysql-client
+
+# installing apache2
+echo -e "${COLOR}---installing Apache---${COLOR_RST}"
+apt-get install -y apache2
+
+# installing php 5.3
+echo -e "${COLOR}---installing php 5.3---${COLOR_RST}"
+apt-get install -y php5 libapache2-mod-php5 php5-mcrypt php5-curl php5-mysql php5-xdebug
+
+#enable mod rewrite for apache2
+echo -e "${COLOR}---enabling rewrite module---${COLOR_RST}"
+if [ ! -f /etc/apache2/mods-enabled/rewrite.load ] ; then
+    a2enmod rewrite
+fi
+
+#deflat module for apache2
+if [ ! -f /etc/apache2/mods-enabled/deflate.load ] ; then
+    a2enmod deflate
+fi
+
+# restart apache2
+echo -e "${COLOR}---restarting apache2---${COLOR_RST}"
+service apache2 restart
